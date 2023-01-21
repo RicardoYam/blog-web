d37e6885a52ad526b74c0adecda747eaa46ffcf0
RicardoYam <lishixun1219@gmail.com>
2023-01-21 21:52:49 +1000

    login & home page changed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,16 @@
-<template lang="">
-    <div>
-        <h1>hi</h1>
+<template>
+    <div class="home-container">
+        <div class="main-container">
+            <el-row :gutter="20">
+                <el-col :span="16">
+                    <div class="grid-content bg-purple"></div>
+                </el-col>
+
+                <el-col :span="8">
+                    <div class="grid-content bg-purple"></div>
+                </el-col>
+            </el-row>
+        </div>
     </div>
 </template>
 
@@ -11,5 +21,27 @@ export default {
 </script>
 
 <style lang="less" scoped>
-    
+.home-container {
+    background-color: aqua;
+    height: 100%;
+}
+
+.main-container {
+    background-color: white;
+    width: 80%;
+    height: 100%;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+}
+
+.bg-purple {
+background: #d3dce6;
+}
+
+.grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
 </style>
---
+++ src/views/Login.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="login-container">
+        <div class="login-box">
+            <el-form class="login-form" lable-position="top">
+                <el-form-item>
+                    <h1>Sign up to system</h1>
+                </el-form-item>
+
+                <!-- 用户名 -->
+                <el-form-item label="Username">
+                    <el-input v-model="username" prefix-icon="iconfont icon-geren"></el-input>
+                </el-form-item>
+
+                <!-- 密码 -->
+                <el-form-item label="Password">
+                    <el-input v-model="password" prefix-icon="iconfont icon-mima"></el-input>
+                </el-form-item>
+
+                <!-- 按钮 -->
+                <el-form-item class="btns">
+                    <el-button type="primary" size="default" @click="">Sign In</el-button>
+                    <el-button type="info" size="default" @click="">Sign Up</el-button>
+                </el-form-item>
+            </el-form>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                username: '',
+                password: ''
+            }
+        }
+    }
+</script>
+
+<style lang="less" scoped>
+.login-container {
+    background-color: aqua;
+    height: 100%;
+}
+
+.login-box {
+    background-color: white;
+    width: 35%;
+    height: 35%;
+    border-radius: 10px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+
+    .login-form {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        width: 60%;
+
+        .btns {
+            display: flex;
+            justify-content: flex-end;
+        }
+    }
+}
+</style>
