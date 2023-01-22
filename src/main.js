5c7b0a97593c76089bdc0619dcae147d17e0f9a1
RicardoYam <lishixun1219@gmail.com>
2023-01-22 22:44:36 +1000

    home & login page changed

--- src/main.js
+++ src/main.js
@@ -1,13 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import ElementUI from 'element-ui'
+import axios from 'axios'
+import 'element-ui/lib/theme-chalk/index.css'
 import './assets/css/gloable.css'
 import './assets/css/iconfont/iconfont.css'
 
+Vue.prototype.$http = axios
 Vue.use(ElementUI);
 Vue.config.productionTip = false
+
+// 服务器根路径
+axios.defaults.baseURL = 'http://localhost:8001/blog'
 
 new Vue({
   router,
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,24 +1,24 @@
 <template>
     <div class="login-container">
         <div class="login-box">
-            <el-form class="login-form" lable-position="top">
+            <el-form v-model="loginForm" class="login-form" lable-position="top">
                 <el-form-item>
                     <h1>Sign up to system</h1>
                 </el-form-item>
 
                 <!-- 用户名 -->
                 <el-form-item label="Username">
-                    <el-input v-model="username" prefix-icon="iconfont icon-geren"></el-input>
+                    <el-input v-model="loginForm.useranme" prefix-icon="iconfont icon-geren"></el-input>
                 </el-form-item>
 
                 <!-- 密码 -->
                 <el-form-item label="Password">
-                    <el-input v-model="password" prefix-icon="iconfont icon-mima"></el-input>
+                    <el-input v-model="loginForm.password" prefix-icon="iconfont icon-mima" type="password"></el-input>
                 </el-form-item>
 
                 <!-- 按钮 -->
                 <el-form-item class="btns">
-                    <el-button type="primary" size="default" @click="">Sign In</el-button>
+                    <el-button type="primary" size="default" @click="login">Sign In</el-button>
                     <el-button type="info" size="default" @click="">Sign Up</el-button>
                 </el-form-item>
             </el-form>
@@ -30,8 +30,15 @@
     export default {
         data() {
             return {
-                username: '',
-                password: ''
+                loginForm: {
+                    useranme: '',
+                    password: ''
+                }
+            }
+        },
+        methods: {
+            login() {
+                
             }
         }
     }
