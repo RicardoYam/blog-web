9f85e59dd1b5012769743c881db79047762e88fc
RicardoYam <10678784+ricardoyam@user.noreply.gitee.com>
2023-01-24 16:11:02 +1000

    home's main content changed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,45 @@
 <template>
     <el-container class="home-container">
 
-        <!-- 头部 -->
-        <el-header>
-            <!-- 标题 -->
-            <div>
-                <span>DuoDuo</span>
-            </div>
-
+        <div class="banner">
             <!-- 导航 -->
-            <el-menu mode="horizontal" background-color="pink" text-color="#ffffff">
+            <el-menu mode="horizontal" background-color="transparent" text-color="#ffffff">
                 <el-menu-item index="1"><i class="iconfont icon-sousuo"></i><span slot="title">Home</span></el-menu-item>
                 <el-menu-item index="2"><i class="iconfont icon-dingwei"></i><span slot="title">Life</span></el-menu-item>
                 <el-menu-item index="3"><i class="iconfont icon-fenlei"></i><span slot="title">Discovery</span></el-menu-item>
                 <el-menu-item index="4"><i class="iconfont icon-shequ"></i><span slot="title">About Me</span></el-menu-item>
                 <el-menu-item index="5"><i class="iconfont icon-geren"></i><span slot="title">Login</span></el-menu-item>
             </el-menu>
-        </el-header>
-        <el-main>Main</el-main>
-        <el-footer>Footer</el-footer>
+
+            <div class="banner-title">
+                <h1>
+                    Ricardo's Blog
+                </h1>
+            </div>
+        </div>
+        
+
+        <div class="main-content">
+            <el-row :gutter="6">
+                <el-col :span="16">
+                    <el-card shadow="always" :body-style="{ padding: '20px' }">
+                        <div slot="header">
+                            <span>hello</span>
+                        </div>
+                        <!-- card body -->
+                    </el-card>
+                </el-col>
+
+                <el-col :span="8">
+                    <el-card shadow="always" :body-style="{ padding: '20px' }">
+                        <div slot="header">
+                            <span>hello</span>
+                        </div>
+                        <!-- card body -->
+                    </el-card>
+                </el-col>
+            </el-row>
+        </div>
     </el-container>
 
     <!-- <div class="home-container">
@@ -43,42 +64,54 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.el-header {
-    background-color: pink;
-    opacity: 0.2;
+.home-container {
+    height: 100%;
+    width: 100%;
     display: flex;
-    justify-content: space-between;
-    align-items: center;
-    font-size: 20px;
-    color: white;
+    justify-content: center;
+    flex-wrap: wrap;
 
-    .el-menu {
+    .banner {
+        background-image:linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url(https://images.pexels.com/photos/1083822/pexels-photo-1083822.jpeg?cs=srgb&dl=pexels-lisa-1083822.jpg&fm=jpg);
+        height: 100%;
+        width: 100%;
+        background-size: cover;
+        background-position: center;
+    
+        .el-menu {
+            width: 100%;
+            opacity: 0.2;
+            border-bottom: 0 !important;
+            display: flex;
+            justify-content: flex-end;
 
-        .el-menu-item {
-            margin-right: 4px;
-            align-content: center;
-            font-size: 12px;
-
-            > i {
+            .el-menu-item {
                 margin-right: 4px;
-                align-items: center;
+                align-content: center;
+                font-size: 12px;
+
+                > i {
+                    margin-right: 4px;
+                    align-items: center;
+                }
             }
         }
+
+        .banner-title {
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%, -50%);
+            color: white;
+        }
+    }
+
+    .main-content {
+        width: 70%;
     }
 }
 
-.el-main {
-    background-color: white;
-}
 
-.el-footer {
-    background-color: grey;
-}
-
-
-.home-container {
-    height: 100%;
-}
 
 // .main-container {
 //     background-color: white;
